67247a42621bfd224fc0c406e05d0e19747e7e03
Nathan Gracia <45590419+NathanGracia@users.noreply.github.com>
2019-02-21 10:48:35 +0100

    Update DateUtils.java

--- fr.imie.bank-master/src.main.java/fr/imie/bank/DateUtils.java
+++ fr.imie.bank-master/src.main.java/fr/imie/bank/DateUtils.java
@@ -7,12 +7,15 @@ public class DateUtils {
 
 	public static final DateTimeFormatter fr = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-	public static LocalDate toDate(String text) {
-		// TODO return ...
+	public static LocalDate toDate(String ddn) {
+		LocalDate birth = LocalDate.parse(ddn, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		return birth;
 	}
 
-	public static String toString(LocalDate date) {
-		// TODO return ...
+	public static String toString(LocalDate ddn) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");
+		String formattedString = ddn.format(formatter);
+		return formattedString;
 	}
 
 }
