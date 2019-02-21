22d62a7d808fe6a8d67406b1665582cde634ed8f
simonbourrasseau <45590458+simonbourrasseau@users.noreply.github.com>
2019-02-21 14:11:56 +0100

    Add files via upload

--- fr.imie.bank-master/src.main.java/fr/imie/bank/DateUtils.java
+++ fr.imie.bank-master/src.main.java/fr/imie/bank/DateUtils.java
@@ -7,14 +7,15 @@ public class DateUtils {
 
 	public static final DateTimeFormatter fr = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-	public static LocalDate toDate(String ddn) {
-		LocalDate birth = LocalDate.parse(ddn, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+	public static LocalDate toDate(String date) {
+		LocalDate birth = LocalDate.parse(date, fr);
 		return birth;
 	}
 
-	public static String toString(LocalDate ddn) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");
-		String formattedString = ddn.format(formatter);
+	public static String toString(LocalDate date) {
+		//DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy"); //(ex: 21 fevrier 2019)
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy"); //(ex: 21/02/2019)
+		String formattedString = date.format(formatter);
 		return formattedString;
 	}
 
