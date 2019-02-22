64ce57b20a09d2b0bb9dc9c00f86a95cc6c1f3e8
simonbourrasseau <simon.bourrasseau@imie.fr>
2019-02-22 12:19:45 +0100

    voila1

--- fr.imie.bank-master/src.main.java/fr/imie/bank/MainConsole.java
+++ fr.imie.bank-master/src.main.java/fr/imie/bank/MainConsole.java
@@ -1,8 +1,12 @@
 package fr.imie.bank;
-import fr.imie.bank.model.BankAccount;  
+
+import fr.imie.bank.model.BankAccount;
 import fr.imie.bank.model.Person;
-import fr.imie.bank.DateUtils;
 import fr.imie.bank.model.PersonDao;
+import fr.imie.bank.DateUtils;
+import fr.imie.bank.model.BankAccountDao;
+import fr.imie.bank.model.PersonDaoCsvImpl;
+import fr.imie.bank.model.BankAccountDaoCsvImpl;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -16,138 +20,162 @@ public class MainConsole {
 		Scanner scanner = new Scanner(System.in);
 		PersonDao persondao = new PersonDao();
 		int choice = 0;
-		while(choice != 5) {
-		System.out.println("1) Lister les personnes");
-		System.out.println("2) Ajouter une personne");
-		System.out.println("3) Modifier une personne");
-		System.out.println("4) Supprimer une personne");
-		System.out.println("5) Lister les comptes");
-		System.out.println("6) Ajouter un compte");
-		System.out.println("7) Modifier un compte");
-		System.out.println("8) Supprimer un compte");
-		System.out.println("9) Quitter");
-		
-		choice = scanner.nextLine().charAt(0);
-        switch(choice) {
-       case '1' : 
-        	for (int i=0; i<Person.getList_person().size();i++) {
-        	Person p1 = (Person) Person.getList_person().get(i);	
-        	LocalDate date2 = p1.getBirthDate();
-        	String date3 = DateUtils.toString(date2);
-  
-             System.out.println("Votre ID est : "+i+"\n"+p1.getFirstName()+" "+p1.getLastName()+" "+p1.getEmail()+" "+date3);          
-            }
-        break;
-        case '2' : 
-        	New_Person();
-        break;
-        case '3':
-        	System.out.println("Saisissez id de la personne a modifier :");
-            
-            String mod = scanner.nextLine();
-            int id = Integer.parseInt(mod);
-            
-            System.out.println("Saisissez votre prÃ©nom :");
-            String firstName = scanner.nextLine();
-            System.out.println("Saisissez votre nom :");
-            String lastName = scanner.nextLine();        
-            System.out.println("Saisissez votre email :");
-            String email = scanner.nextLine();        
-            System.out.println("Saisissez votre date de naissance");
-            String birthDate = scanner.nextLine();
-    		try {
-    			LocalDate birth = DateUtils.toDate(birthDate);
-    			
-    			Person p1 = new Person(firstName, lastName, email, birth);
-    			Person.getList_person();
-    			
-    			} catch (Exception e) {
-    				boolean DateTimeParseException = false;
-    				while (DateTimeParseException  != true) {
-    					Scanner scanner1 = new Scanner(System.in);
-    					System.out.println("Saisissez une date de naissance VALIDE (dd/MM/yyyy) : ");
-    					String birthDate1 = scanner1.nextLine();
-    					try {
-    					LocalDate birth2 = DateUtils.toDate(birthDate1);
-    					DateTimeParseException = true;
-    					
-    					Person p1 = new Person(firstName, lastName, email, birth2);
-    					Person.setList_person(id, p1);
-    					} catch(Exception f) {
-    					DateTimeParseException = false;	
-    				}
-    			}
-    			}
-            
-        	break;
-        case '4':
-            System.out.println("quel est l'ID de la personne a supprimer ?");
-            String id2 = scanner.nextLine();
-            int id1 = Integer.parseInt(id2);
-            Person.getList_person().get(id1);
-            Person.getList_person().remove(id1);
-            System.out.println("DonnÃ©es de la personne detruite !");
-        break;
-        case '5':
-        	 System.out.println("quel est l'ID de la personne Ã  consulter ?");
-             String id4 = scanner.nextLine();
-             int id3 = Integer.parseInt(id4);
-            Person p = Person.getList_person().get(id3);
-            for (int i=0; i<p.getList_accounts().size();i++) {
-                BankAccount b1 = (BankAccount) p.getList_accounts().get(i);
-                System.out.println("ID du compte : "+i+" Ton/Tes numÃ©ro(s) de compte(s) est/sont : "+b1.getNumber()+"\n");
-            }
-        break;
-        case '6':System.out.println("quel est l'ID de la personne Ã  consulter ?");
-        String id10 = scanner.nextLine();
-        int id9 = Integer.parseInt(id10);
-        Person p4 = Person.getList_person().get(id9);
-        System.out.println("Saisissez un numÃ©ro de compte Ã  crÃ©er :");
-        String nc = scanner.nextLine();
-        BankAccount b = new BankAccount(nc);
-        p4.addBankAccount(b);
-        
-        break;
-        case '7':
-        	System.out.println("quel est l'ID de la personne Ã  consulter ?");
-            String id8 = scanner.nextLine();
-            int id7 = Integer.parseInt(id8);
-            Person p3 = Person.getList_person().get(id7);
-            System.out.println("Saisissez l'ID du compte que vous voulez modifiez :");
+		while (choice != 5) {
+			System.out.println("1) Lister les personnes");
+			System.out.println("2) Ajouter une personne");
+			System.out.println("3) Modifier une personne");
+			System.out.println("4) Supprimer une personne");
+			System.out.println("5) Lister les comptes");
+			System.out.println("6) Ajouter un compte");
+			System.out.println("7) Modifier un compte");
+			System.out.println("8) Supprimer un compte");
+			System.out.println("9) Quitter");
 
-            String modc = scanner.nextLine();
-            int idc = Integer.parseInt(modc);
-            System.out.println("Saisissez votre nouveau numÃ©ro de compte :");
-            String nnb = scanner.nextLine();
+			choice = scanner.nextLine().charAt(0);
+			switch (choice) {
+			case '1':
+				for (int i = 0; i < Person.getList_person().size(); i++) {
+					Person p1 = (Person) Person.getList_person().get(i);
+					LocalDate date2 = p1.getBirthDate();
+					String date3 = DateUtils.toString(date2);
 
-            BankAccount b2 = new BankAccount(nnb);
-            p3.getList_accounts().set(idc, b2);
+					System.out.println("Votre ID est : " + i + "\n" + p1.getFirstName() + " " + p1.getLastName() + " "
+							+ p1.getEmail() + " " + date3);
+				}
+				break;
+			case '2':
+				New_Person();
+				break;
+			case '3':
+				System.out.println("Saisissez id de la personne a modifier :");
 
-            break;
-        	case '8':
-        		System.out.println("quel est l'ID de la personne Ã  consulter ?");
-                String id6 = scanner.nextLine();
-                int id5 = Integer.parseInt(id6);
-               Person p1 = Person.getList_person().get(id5);
-                System.out.println("Saisissez le compte que vous voulez supprimer :");
-                String sidc = scanner.nextLine();
+				String mod = scanner.nextLine();
+				int id = Integer.parseInt(mod);
 
-                int nsid = Integer.parseInt(sidc);
+				try {
+					Person id1 = PersonDao.findById(id);
+					System.out.println("ID correct");
+				} catch (Exception g) {
 
-                p1.getList_accounts().get(nsid);
-                p1.getList_accounts().remove(nsid);
+					boolean lid = false;
+					while (lid != true) {
+						System.out.println("ID incorrect");
+						System.out.println("Saisissez un id correct de la personne a modifier :");
+						String mod2 = scanner.nextLine();
+						try {
+							int modid = Integer.parseInt(mod2);
+							Person modid1 = PersonDao.findById(modid);
+							lid = true;
+						} catch (Exception h) {
+							lid = false;
+						}
+					}
+				}
 
-            break;
+				System.out.println("Saisissez votre prénom :");
+				String firstName = scanner.nextLine();
+				System.out.println("Saisissez votre nom :");
+				String lastName = scanner.nextLine();
+				System.out.println("Saisissez votre email :");
+				String email = scanner.nextLine();
+				System.out.println("Saisissez votre date de naissance");
+				String birthDate = scanner.nextLine();
+				try {
+					LocalDate birth = DateUtils.toDate(birthDate);
 
-            case '9':
-            System.exit(1);
-            break;
-        		}
-		}   
+					Person p1 = new Person(firstName, lastName, email, birth);
+					Person.changeListPerson(id, p1);
+
+				} catch (Exception e) {
+					boolean DateTimeParseException = false;
+					while (DateTimeParseException != true) {
+						Scanner scanner1 = new Scanner(System.in);
+						System.out.println("Saisissez une date de naissance VALIDE (dd/MM/yyyy) : ");
+						String birthDate1 = scanner1.nextLine();
+						try {
+							LocalDate birth2 = DateUtils.toDate(birthDate1);
+							DateTimeParseException = true;
+
+							Person p1 = new Person(firstName, lastName, email, birth2);
+							Person.changeListPerson(id, p1);
+						} catch (Exception f) {
+							DateTimeParseException = false;
+						}
+					}
+				}
+
+				break;
+			case '4':
+				System.out.println("quel est l'ID de la personne a supprimer ?");
+				String id2 = scanner.nextLine();
+				int id1 = Integer.parseInt(id2);
+				Person.getList_person().get(id1);
+				Person.getList_person().remove(id1);
+				System.out.println("Données de la personne detruite !");
+				break;
+			case '5':
+				System.out.println("quel est l'ID de la personne à consulter ?");
+				String id4 = scanner.nextLine();
+				int id3 = Integer.parseInt(id4);
+				Person p = Person.getList_person().get(id3);
+				for (int i = 0; i < p.getList_accounts().size(); i++) {
+					BankAccount b1 = (BankAccount) p.getList_accounts().get(i);
+					System.out.println("ID du compte : " + i + " Ton/Tes numéro(s) de compte(s) est/sont : "
+							+ b1.getNumber() + "\n");
+				}
+				break;
+			case '6':
+				System.out.println("quel est l'ID de la personne à consulter ?");
+				String id10 = scanner.nextLine();
+				int id9 = Integer.parseInt(id10);
+				Person p4 = Person.getList_person().get(id9);
+				System.out.println("Saisissez un numéro de compte à créer :");
+				String nc = scanner.nextLine();
+				BankAccount b = new BankAccount(nc);
+				p4.addBankAccount(b);
+
+				break;
+			case '7':
+				System.out.println("quel est l'ID de la personne à consulter ?");
+				String id8 = scanner.nextLine();
+				int id7 = Integer.parseInt(id8);
+				Person p3 = Person.getList_person().get(id7);
+				System.out.println("Saisissez l'ID du compte que vous voulez modifiez :");
+
+				String modc = scanner.nextLine();
+				int idc = Integer.parseInt(modc);
+				System.out.println("Saisissez votre nouveau numéro de compte :");
+				String nnb = scanner.nextLine();
+
+				BankAccount b2 = new BankAccount(nnb);
+				p3.getList_accounts().set(idc, b2);
+
+				break;
+			case '8':
+				System.out.println("quel est l'ID de la personne à consulter ?");
+				String id6 = scanner.nextLine();
+				int id5 = Integer.parseInt(id6);
+				Person p1 = Person.getList_person().get(id5);
+				System.out.println("Saisissez le compte que vous voulez supprimer :");
+				String sidc = scanner.nextLine();
+
+				int nsid = Integer.parseInt(sidc);
+
+				p1.getList_accounts().get(nsid);
+				p1.getList_accounts().remove(nsid);
+
+				break;
+
+			case '9':
+				System.exit(1);
+				break;
+			}
+		}
 	}
+
 	private static void New_Person() {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Saisissez un prÃ©nom : ");
+		System.out.println("Saisissez un prénom : ");
 		String firstName = scanner.nextLine();
 		System.out.println("Saisissez un nom : ");
 		String lastName = scanner.nextLine();
@@ -156,27 +184,27 @@ public class MainConsole {
 		System.out.println("Saisissez une date de naissance : ");
 		String birthDate = scanner.nextLine();
 		try {
-		LocalDate birth = DateUtils.toDate(birthDate);
-		
-		Person p = new Person(firstName, lastName, email, birth);
-		Person.addPerson(p);
-		
+			LocalDate birth = DateUtils.toDate(birthDate);
+
+			Person p = new Person(firstName, lastName, email, birth);
+			Person.addPerson(p);
+
 		} catch (Exception e) {
 			boolean DateTimeParseException = false;
-			while (DateTimeParseException  != true) {
+			while (DateTimeParseException != true) {
 				Scanner scanner1 = new Scanner(System.in);
 				System.out.println("Saisissez une date de naissance VALIDE (dd/MM/yyyy) : ");
 				String birthDate1 = scanner1.nextLine();
 				try {
-				LocalDate birth2 = DateUtils.toDate(birthDate1);
-				DateTimeParseException = true;
-				
-				Person p = new Person(firstName, lastName, email, birth2);
-				Person.addPerson(p);
-				} catch(Exception f) {
-				DateTimeParseException = false;	
+					LocalDate birth2 = DateUtils.toDate(birthDate1);
+					DateTimeParseException = true;
+
+					Person p = new Person(firstName, lastName, email, birth2);
+					Person.addPerson(p);
+				} catch (Exception f) {
+					DateTimeParseException = false;
+				}
 			}
-		}
 		}
 	}
 }
--- fr.imie.bank-master/src.main.java/fr/imie/bank/model/Person.java
+++ fr.imie.bank-master/src.main.java/fr/imie/bank/model/Person.java
@@ -6,84 +6,91 @@ import java.util.List;
 import fr.imie.bank.MainConsole;
 
 public class Person extends MainConsole{
-    private String firstName;
-    private String lastName;
-    private String email;
-    private LocalDate birthDate;
-    private static List<Person> list_person = new ArrayList<Person>();
-    private List<BankAccount> list_accounts = new ArrayList<BankAccount>();
-    
-    
-    
-    public  List<BankAccount> getList_accounts() {
-        return list_accounts;
-    }
+	private String firstName;
+	private String lastName;
+	private String email;
+	private LocalDate birthDate;
+	private static List<Person> list_person = new ArrayList<Person>();
+    private static List<BankAccount> list_accounts = new ArrayList<BankAccount>();
+	
+	
+	
+	public static List<BankAccount> getList_accounts() {
+		return list_accounts;
+	}
 
 
+	public static void setList_accounts(List<BankAccount> list_accounts) {
+		Person.list_accounts = list_accounts;
+	}
 
 
-
-    public Person(String firstName, String lastName, String email, LocalDate birthDate) {
-        super();
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.birthDate = birthDate;
+	public Person(String firstName, String lastName, String email, LocalDate birthDate) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.birthDate = birthDate;
 }
 
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
 
-    public void setList_person(List<Person> list_person) {
-        Person.list_person = list_person;
-    }
+	public void setList_person(List<Person> list_person) {
+		Person.list_person = list_person;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
-
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
 
-    public String getEmail() {
-        return email;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
 
-    public static List<Person> getList_person() {
-        return list_person;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-    
-    public static void addPerson(Person p) {
-        list_person.add(p);
-    }
-    public void addBankAccount(BankAccount b) {
+	public static List<Person> getList_person() {
+		return list_person;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	public static void addPerson(Person p) {
+		list_person.add(p);
+	}
+	public static void addBankAccount(BankAccount b) {
         // TODO Auto-generated method stub
-       this.list_accounts.add(b);
+       getList_accounts().add(b);
     }
+	public static void changeListPerson(int i,Person p) {
+		list_person.set(i,p);
+	}
+
+
 }
